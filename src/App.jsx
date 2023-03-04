8beb579239f5b4dc89641aa57d2aa846459ba00a
aldrinmcaalim <aldrinmcaalim@gmail.com>
2023-03-04 18:03:49 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>I like cheese</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias possimus
+        numquam delectus asperiores, doloribus architecto totam corrupti sed
+        nulla provident voluptates. Sit est perspiciatis repellendus. Beatae
+        laborum delectus dolor sit!
+      </p>
+    </>
+  );
+}
+
+export default App;
